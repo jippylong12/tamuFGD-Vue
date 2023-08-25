5015a0041ee01252f87c9bd90c3f29c137395f11
Marcus Salinas <markysalinas12@gmail.com>
2023-08-25 12:53:34 +0300

    Had to update the prod.bat because for some reason after vite build it just exits lol

--- cmd/prod.bat
+++ cmd/prod.bat
@@ -1,6 +1,3 @@
 rem Build our current script and then move those copies to the PHP server locally
 set NODE_ENV=production
-vite build
-powershell Copy-Item -Force dist/index.html C:\wamp64\www\cPanel
-powershell Copy-Item -Force dist/assets/index.css C:\wamp64\www\cPanel\assets
-powershell Copy-Item -Force dist/assets/index.js C:\wamp64\www\cPanel\assets
+vite build && powershell Copy-Item -Force dist/index.html C:\wamp64\www\cPanel && powershell Copy-Item -Force dist/assets/index.css C:\wamp64\www\cPanel\assets && powershell Copy-Item -Force dist/assets/index.js C:\wamp64\www\cPanel\assets
