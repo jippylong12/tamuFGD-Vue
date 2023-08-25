8786597ae9253d09e4d16962168eedd2fb2555bb
Marcus Salinas <markysalinas12@gmail.com>
2023-08-25 13:04:20 +0300

    Refactored SharedButton

--- src/App.vue
+++ src/App.vue
@@ -6,9 +6,10 @@ import Toastify from 'toastify-js'
 import {FilterMatchMode} from 'primevue/api';
 import MainForm from "@/components/MainForm.vue";
 import GeneralInfo from "@/components/GeneralInfo.vue";
+import ShareButton from "@/components/ShareButton.vue";
 
 export default {
-  components: {GeneralInfo, MainForm},
+  components: {ShareButton, GeneralInfo, MainForm},
   setup() {
     const course = ref('')
     const courseNumber = ref('')
@@ -450,19 +451,7 @@ export default {
         </v-col>
       </v-row>
 
-      <div v-if="tableData.length > 0" id="copy-url-button"
-           @click="pressedCopyButton">
-        <button class="copy-url-button">
-          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
-               fill="currentColor" class="bi bi-share" viewBox="0 0 16 16">
-            <path
-                d="M13.5 1a1.5 1.5 0 1 0 0 3 1.5 1.5 0 0 0 0-3zM11 2.5a2.5 2.5 0 1 1 .603 1.628l-6.718 3.12a2.499 2.499 0 0 1 0 1.504l6.718 3.12a2.5 2.5 0 1 1-.488.876l-6.718-3.12a2.5 2.5 0 1 1 0-3.256l6.718-3.12A2.5 2.5 0 0 1 11 2.5zm-8.5 4a1.5 1.5 0 1 0 0 3 1.5 1.5 0 0 0 0-3zm11 5.5a1.5 1.5 0 1 0 0 3 1.5 1.5 0 0 0 0-3z"/>
-          </svg>
-          <span class="ms-1">
-                Share
-            </span>
-        </button>
-      </div>
+      <ShareButton :show="tableData.length > 0" @click="pressedCopyButton"/>
     </v-container>
   </v-app>
 
