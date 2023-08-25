fecd65ea5cd38cfb4e57508b3dd9241c59b40aec
Marcus Salinas <markysalinas12@gmail.com>
2023-08-25 13:00:28 +0300

    Refactored Info

--- src/App.vue
+++ src/App.vue
@@ -5,9 +5,10 @@ import {DEBUG_FLAG} from "./main";
 import Toastify from 'toastify-js'
 import {FilterMatchMode} from 'primevue/api';
 import MainForm from "@/components/MainForm.vue";
+import GeneralInfo from "@/components/GeneralInfo.vue";
 
 export default {
-  components: {MainForm},
+  components: {GeneralInfo, MainForm},
   setup() {
     const course = ref('')
     const courseNumber = ref('')
@@ -383,49 +384,8 @@ export default {
                 v-model:sort-by-value="sortByValue"
                 :sort-by-options=sortByOptions
                 @submit-btn-click="onSubmitButtonClick"/>
+      <GeneralInfo :show="tableData.length === 0 && !dataLoading"/>
 
-      <v-row no-gutters v-if="tableData.length === 0 && !dataLoading">
-        <v-col cols="12" class="py-2">
-          <h5 class="suggestion"> Data from Spring 2012 - Spring 2023</h5>
-          <h5 class="suggestion"><a
-              href="mailto: jippylong12@gmail.com">Email</a> for suggestions
-          </h5>
-          <h5 class="suggestion"><a href="http://www.jippylong12.xyz">Me</a>
-          </h5>
-        </v-col>
-
-        <v-col cols="12" class="px-4 py-4">
-          <p class="text-center"> I want this to be helpful to all
-            students. </p>
-          <p class="text-center">An education is the best indicator of success,
-            but you don't have to be dumb going about getting one.</p>
-          <p class="text-center mb-4"> I will keep updating the course data as
-            long as I can.</p>
-          <ul class="text-center">
-            <li><a href="https://github.com/jippylong12/TAMU_FGD_FRONT_END">
-              Front-end Code</a></li>
-            <li><a href="https://github.com/jippylong12/TAMU_FGD">Server
-              Code</a></li>
-            <li><a
-                href="https://drive.google.com/drive/folders/0B6WlnfAGiKk9ZlEwcElEZW9rUE0?resourcekey=0-bkINHRtazSza6dDL8MIyIg&usp=sharing">
-              CSV DB Files</a></li>
-          </ul>
-        </v-col>
-
-        <v-col cols="12" class="py-2">
-          <p style="text-align: center"><b>I'm glad this website is helping
-            people. </b></p>
-        </v-col>
-        <v-col cols="12">
-          <h5 class="suggestion"> Thanks & Acknowledgements</h5>
-          <h5 class="credits">
-            <b>My Momma: #1</b><br>
-            <b>Matthew:</b> adding the Q drop column.<br>
-            <b>Zubin:</b> Pointing out the averaging problem.<br>
-            <b>Eric:</b> Suggesting to distinguish honors sections.
-          </h5>
-        </v-col>
-      </v-row>
       <v-row v-if="tableData.length > 0 || dataLoading" class="px-4 mb-4">
         <v-col cols="12">
           <DataTable :loading="dataLoading" :rowClass="determineRowClass"
