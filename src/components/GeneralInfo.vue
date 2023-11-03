dc356891222e66bd021f0918ac8869be48762ce0
Marcus Salinas <markysalinas12@gmail.com>
2023-11-04 02:09:35 +0400

    Simplifying page

--- src/components/GeneralInfo.vue
+++ src/components/GeneralInfo.vue
@@ -5,12 +5,12 @@ defineProps(['show'])
 <template>
   <v-row no-gutters v-if="show">
     <v-col cols="12" class="py-2">
+      <h5 class="suggestion"><a href="https://www.jippylong12.xyz">Me</a>
       <h5 class="suggestion"> Data from Spring 2012 - Spring 2023</h5>
-      <h5 class="suggestion"><a href="http://www.jippylong12.xyz">Me</a>
       </h5>
     </v-col>
 
-    <v-col cols="12" class="px-4 py-4">
+    <v-col cols="12" class="px-4 pb-4">
       <p class="text-center"> I want this to be helpful to all
         students. </p>
       <p class="text-center">An education is the best indicator of success,
